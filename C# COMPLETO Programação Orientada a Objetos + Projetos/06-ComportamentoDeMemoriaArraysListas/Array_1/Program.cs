594824e59edf9fa509f7d1ffef3a109331d2c7a5
Felipe Kal <felipe.kal@outlook.com>
2024-11-30 14:40:43 -0300

    Update

--- C# COMPLETO Programação Orientada a Objetos + Projetos/06-ComportamentoDeMemoriaArraysListas/Array_1/Program.cs
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/06-ComportamentoDeMemoriaArraysListas/Array_1/Program.cs
@@ -7,4 +7,24 @@
 //element - 0 : 1
 //element - 1 : 1
 //element - 2 : 2
+//.......
+//Expected Output :
+//Elements in array are: 1 1 2 3 4 5 6 7 8 9
+
+Console.WriteLine("Input 10 elements in the array: ");
+
+int[] vet = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Element - {i} : ");
+    int n = int.Parse(Console.ReadLine());
+    vet[i] = n;
+}
+
+Console.Write($"Elements in array are: ");
+for (int i = 0;i < 10; i++)
+{
+    Console.Write($" {vet[i]} ");
+}
 
---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/08-Enumeracoes e composicao/Exercises/Program.cs
@@ -0,0 +1,46 @@
+﻿using Exercises.Entities;
+using Exercises.Entities.Enums;
+using System.Globalization;
+
+Console.Write("Enter department name: ");
+string deptName = Console.ReadLine();
+
+Console.Write("Name: ");
+string name = Console.ReadLine();
+
+Console.Write("Level (Junior/MidLevel/Senior): ");
+WorkerLevel workerLevel = Enum.Parse<WorkerLevel>(Console.ReadLine());
+
+Console.Write("base Salary: ");
+double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Department dept = new Department(deptName);
+
+Worker worker = new Worker(name, workerLevel, baseSalary, dept);
+
+Console.Write("How many contracts to this worker? ");
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"Enter #{i} contract data: ");
+    Console.Write("Date (DD/MM/YYYY): ");
+    DateTime date = DateTime.Parse(Console.ReadLine());
+    Console.Write("Value per hour: ");
+    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    Console.Write("Duration (hours): ");
+    int hours = int.Parse(Console.ReadLine());
+    HourContract contract = new HourContract(date, valuePerHour, hours);
+    worker.AddContract(contract);
+}
+
+Console.WriteLine();
+
+Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+string monthAndYear = Console.ReadLine();
+int month = int.Parse(monthAndYear.Substring(0, 2));
+int year = int.Parse(monthAndYear.Substring(3));
+Console.WriteLine("Name: " + worker.Name);
+Console.WriteLine("Department: " + worker.Department.Name);
+Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month));
+
