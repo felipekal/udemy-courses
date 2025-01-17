1f66f86cfd8cfd03e886dc9939f1adcc374f7b1c
Felipe Kal <felipe.kal@outlook.com>
2025-01-17 12:48:20 -0300

    generics pt1

---
+++ C# COMPLETO Programação Orientada a Objetos + Projetos/13-Generics,Set,Dictionary/generics2/Entities/CalculationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics2.Entities
+{
+    internal class CalculationService<T> where T : IComparable
+    {
+        public int Max(List<T> list)
+        {
+            if (list.Count == 0) throw new ArgumentException("The list cannot be empty");
+
+            T max = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i] > max) max = list[i];
+            }
+            return max;
+        }
+    }
